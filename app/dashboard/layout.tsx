13db7bac6f3eb084da11634ebd547748dbd7dd7e
Connor Smith <connorcja@gmail.com>
2023-09-08 16:24:19 -0400

    delete holdover files

--- app/dashboard/layout.tsx
+++
@@ -1,5 +0,0 @@
-import {ReactNode} from "react";
-
-export default function DashboardLayout(props: {children: ReactNode}){
-    return <body>{props.children}</body>
-}
--- app/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Page(){
-    return (<>
-    <h1>Dashaboard</h1>
-    <nav>Input Selection</nav>
-    </>)
-}
